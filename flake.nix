402d22662110277a45371d4b33c97f79db481348
Sebastian Nobs <sebastian.ammiel@gmail.com>
2026-05-16 18:44:14 +0200

    added readme

--- flake.nix
+++ flake.nix
@@ -1,22 +1,35 @@
 {
-  description = "Custom Packages";
+  description = "My Team's Custom Nix Packages";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in {
-      packages.${system} = {
-        openfoam-13 = pkgs.callPackage ./openfoam-13/default.nix { };
-        default = self.packages.${system}.openfoam-13;
-      };
+      
+      packages = forAllSystems (system:
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          openfoam-13 = pkgs.callPackage ./openfoam-13/default.nix { };
+          default = self.packages.${system}.openfoam-13;
+        }
+      );
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          self.packages.${system}.openfoam-13
-        ];
-      };
+      devShells = forAllSystems (system:
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = [
+              self.packages.${system}.openfoam-13
+            ];
+          };
+        }
+      );
+
     };
 }
