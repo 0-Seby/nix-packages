8d04ec0ec53bb54f5d4b3baceb9336ce8bf27638
Sebastian Nobs <sebastian.ammiel@gmail.com>
2026-05-19 20:42:11 +0200

    overlay

--- flake.nix
+++ flake.nix
@@ -1,13 +1,10 @@
 {
   description = "My Team's Custom Nix Packages";
-
   nixConfig = {
     extra-substituters = [ "https://seby.cachix.org" ];
     extra-trusted-public-keys = [ "seby.cachix.org-1:Vych8bxZ7KpUVrz2GELTegGr7th/kdAWHfzVVENyocc=" ];
   };
-
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-
   outputs =
     { self, nixpkgs }:
     let
@@ -17,20 +14,21 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-
       forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      pythonPackagesOverlay = pyFinal: pyPrev: {
+        multicollections = pyFinal.callPackage ./multicollections/default.nix { };
+
+        foamlib = pyFinal.callPackage ./foamlib/default.nix {
+          multicollections = pyFinal.multicollections;
+        };
+
+        CoolProp = pyFinal.callPackage ./coolprop/default.nix { };
+      };
 
       overlay = final: prev: {
         python3 = prev.python3.override {
-          packageOverrides = pyFinal: pyPrev: rec {
-            multicollections = pyFinal.callPackage ./multicollections/default.nix { };
-
-            foamlib = pyFinal.callPackage ./foamlib/default.nix {
-              multicollections = pyFinal.multicollections;
-            };
-
-            CoolProp = pyFinal.callPackage ./coolprop/default.nix { };
-          };
+          packageOverrides = pythonPackagesOverlay;
         };
       };
 
@@ -53,9 +51,16 @@
           ps.multicollections
           ps.CoolProp
         ]);
-
     in
     {
+      overlays = {
+        default = overlay;
+      };
+
+      lib = {
+        pythonPackagesOverlay = pythonPackagesOverlay;
+      };
+
       packages = forAllSystems (
         system:
         let
@@ -68,7 +73,6 @@
           coolprop = pkgs.python3.pkgs.CoolProp;
         }
       );
-
       devShells = forAllSystems (
         system:
         let
@@ -89,7 +93,6 @@
           };
         }
       );
-
       checks = forAllSystems (
         system:
         let
@@ -99,16 +102,12 @@
         {
           python-imports = pkgs.stdenv.mkDerivation {
             name = "python-import-checks";
-
             buildInputs = [ python ];
-
             dontUnpack = true;
-
             buildPhase = ''
               echo "Running Python import tests..."
               ${python}/bin/python ${./tests/imports.py}
             '';
-
             installPhase = ''
               mkdir -p $out
               touch $out/done
@@ -117,5 +116,4 @@
         }
       );
     };
-
 }
