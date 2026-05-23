791508856c7f2779bbec0c2c5f93313f1f99e7c0
Sebastian Nobs <sebastian.ammiel@gmail.com>
2026-05-23 16:48:05 +0200

    some fish openfoam improvements

--- openfoam/generate-fish-env.sh
+++ openfoam/generate-fish-env.sh
@@ -5,12 +5,11 @@
 # Inputs: $out (current derivation's output path)
 set -euo pipefail
 
-# 1) `declare -x FOO="bar"` -> `set -gx FOO "bar"`
-# 2) PATH is a list in fish, not a colon-string
 @sed@ -n \
     -e 's|^declare -x \([A-Za-z_][A-Za-z0-9_]*\)="\(.*\)"$|set -gx \1 "\2"|p' \
     "$out/etc/openfoam-env.sh" \
   | @sed@ \
+    -e 's|\${\([A-Za-z_][A-Za-z0-9_]*\)}|$\1|g' \
     -e 's|^set -gx PATH "\(.*\)"$|set -gx PATH (string split ":" "\1")|' \
     -e 's|^set -gx LD_LIBRARY_PATH "\(.*\)"$|set -gx LD_LIBRARY_PATH (string split ":" "\1")|' \
     > "$out/etc/openfoam-env.fish"
